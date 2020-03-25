ec563dd0e1f366b3f4681e19aac3c2feaee8f28f
kiat-mao <kiat.mao@gmail.com>
2020-03-25 13:34:44 +0800

    add local timezone

--- config/application.rb
+++ config/application.rb
@@ -16,5 +16,8 @@ module ShpostMailMessage
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
      config.i18n.default_locale = "zh-CN"
+
+    config.active_record.default_timezone = :local
+    config.time_zone = 'Beijing'  
   end
 end
